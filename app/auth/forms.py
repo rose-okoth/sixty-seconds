967d84824b376236e049acdf55d86eca9b0dad93
rose-okoth <okoth.rose0@gmail.com>
2021-03-02 17:37:43 +0300

    change navbar font size

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,6 @@ class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     firstname = StringField('Enter your first name',validators = [Required()])
-    lastname = StringField('Enter your last name',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
--- app/auth/views.py
+++ app/auth/views.py
@@ -24,7 +24,7 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
